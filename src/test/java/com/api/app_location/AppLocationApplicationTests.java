4513699f26ff29f9e7e9b008473134a70eca387f
Leonardo Pires <leonardo.carvalho.pires@gmail.com>
2025-10-18 18:42:48 -0300

    teste

--- src/test/java/com/api/app_location/AppLocationApplicationTests.java
+++ src/test/java/com/api/app_location/AppLocationApplicationTests.java
@@ -1,13 +1,25 @@
 package com.api.app_location;
 
+import com.api.app_location.dao.CoffeWorkRepository;
+import com.api.app_location.entity.CoffeWork;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class AppLocationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    CoffeWorkRepository coffeWorkRepository = Mockito.mock(CoffeWorkRepository.class);
+
+    @Test
+    void deveSalvarEBuscarCoffe() {
+
+    }
 
 }
