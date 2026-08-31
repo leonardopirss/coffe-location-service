63e6fd9fa716ec3bbe220610640d01b2e0270c68
leonardopires <leonardo.carvalho.pires@gmail.com>
2026-08-30 21:01:59 -0300

    mini kube configurations

---
+++ src/main/java/com/api/app_location/config/OpenApiConfiguration.java
@@ -0,0 +1,31 @@
+package com.api.app_location.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI appLocationOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("App Location Service API")
+                        .description("Documentacao da API do App Location Service")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
